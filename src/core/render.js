5a0f55b262bd528c397be219447290faf0b8c8a2
WebDev <b16edu@gmail.com>
2025-05-25 17:47:49 +0300

    Fix render to support number nodes

--- src/core/render.js
+++ src/core/render.js
@@ -1,12 +1,21 @@
 export function render(element, container) {
-  const node = typeof element === 'string' ? document.createTextNode(element) : document.createElement(element.type);
+    
+  const node = (typeof element === 'string' || typeof element === 'number') 
+  ? document.createTextNode(element) 
+  : document.createElement(element.type);
 
-  if (typeof element !== 'string') {
+
+  if (typeof element !== 'string' && typeof element !== 'number') {
     Object.entries(element.props || {}).forEach(([key, value]) => {
       if (key.startsWith('on') && typeof value === 'function') {
         node.addEventListener(key.slice(2).toLowerCase(), value);
       } else {
-        node.setAttribute(key, value);
+        if (key === 'className') {
+  node.setAttribute('class', value);
+} else {
+  node.setAttribute(key, value);
+}
+
       }
     });
 
